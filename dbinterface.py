afaf786bf30ca89b62daf880c88925f170957629
Gus Smith <hfs5022@psu.edu>
2016-03-07 09:44:51 -0500

    adds add table statement, close function, start of adduser function

--- dbinterface.py
+++ dbinterface.py
@@ -3,4 +3,22 @@ import os
 
 class DbInterface:
 	def __init__(self, db_path):
+		
+		# open connection
 		self.conn = sqlite3.connect(db_path)
+		
+		# create user table
+		cur =  self.conn.cursor()
+		cur.execute("CREATE TABLE IF NOT EXISTS users " +
+								"(user_id INTEGER, " +
+								"reputation INTEGER, " +
+								"CONSTRAINT users_pk PRIMARY KEY user_id)")
+		
+		# don't forget to commit!
+		self.conn.commit()
+								
+	def close(self):
+		self.conn.close()
+		
+	def adduser(self, userid, startingrep):
+		cur = self.conn.cursor()
