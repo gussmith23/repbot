457a42df0b802e84a75e2fe2e0eefe425f1cb7a4
Gus Smith <hfs5022@psu.edu>
2016-03-08 08:58:14 -0500

    adds worker for new threaded producer-consumer structure

--- dbinterface.py
+++ dbinterface.py
@@ -1,25 +1,18 @@
 import sqlite3
 import os
 import time
+import queue
+import threading
 
 class DbInterface:
 	def __init__(self, db_path):
+				
+		self.q = queue.Queue()
 		
-		# open connection
-		self.conn = sqlite3.connect(db_path)
+		# setup&start thread that will handle db accesses
+		t = threading.Thread(target = worker)
+		t.start()
 		
-		# create user table
-		cur =  self.conn.cursor()
-		cur.execute("CREATE TABLE IF NOT EXISTS users " +
-								"(user_id INTEGER, " +
-								"reputation INTEGER, " +
-								"username TEXT," +
-								"time_joined INTEGER," +
-								"CONSTRAINT users_pk PRIMARY KEY (user_id))")
-		
-		# don't forget to commit!
-		self.conn.commit()
-								
 	def close(self):
 		self.conn.close()
 		
@@ -61,4 +54,65 @@ class DbInterface:
 		
 		self.setrep(userid, self.getrep(userid) + amount)
 		
-									
+	def worker(self):
+	
+		# open connection. this object should ONLY be touched
+		# by this worker loop.
+		# TODO: remove 'self.' so that it's local to this loop.
+		self.conn = sqlite3.connect(db_path)
+		
+		# create user table
+		cur =  self.conn.cursor()
+		cur.execute("CREATE TABLE IF NOT EXISTS users " +
+								"(user_id INTEGER, " +
+								"reputation INTEGER, " +
+								"username TEXT," +
+								"time_joined INTEGER," +
+								"CONSTRAINT users_pk PRIMARY KEY (user_id))")
+		
+		# don't forget to commit!
+		self.conn.commit()
+		
+		while True:
+			query = q.get()
+			if query is None: continue
+			
+			# query structure will be a tuple with the following items:
+			# first, a string query (required)
+			# second, a tuple of fill-in arguments for the query (required, can be empty)
+			# third, an empty list for getting return values. (required even if no returns given)
+			# TODO i don't like this structure; second and third shouldn't be required
+			
+			cur = self.conn.cursor()
+			
+			error = False
+			
+			# executing with arguments
+			if len(query) > 1:
+				try:	
+					cur.execute(query[0], query[1])
+				except sqlite3.IntegrityError:
+					error = True
+				
+			# executing without arguments
+			else:
+				try:	
+					cur.execute(query[0])
+				except sqlite3.IntegrityError:
+					error = True				
+					
+			# handle error case and exit if error 
+			if error:
+				query[2].extend([False])
+				q.task_done()
+				continue
+					
+			# if there's no return, we signal with "None"
+			all = cur.fetchall()
+			if (len(all) == 0): 
+				q[2].extend([None])
+			else:	
+				q[2].extend(all)
+			
+			q.task_done()
+				
